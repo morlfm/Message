91e1667947436b0b784c45e3ec230595d0ef59bf
morlfm <72264704+morlfm@users.noreply.github.com>
2022-03-10 16:28:12 -0300

    Update validators.rb

--- transactions_producer/validators.rb
+++ transactions_producer/validators.rb
@@ -3,13 +3,11 @@ require 'cpf_cnpj'
 class Validator
   def validate_cpf(cpf)
     valid = CPF.valid?(cpf, strict: true)
-
     raise 'CPF invalid' unless valid
   end
 
   def validate_value(value)
     valid = value.is_a? Float
-
     raise 'Invalid value' unless valid
   end
 
